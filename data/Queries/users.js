157a200be10ab9dfce0d32182ca1b9ac6508b9eb
LDEV <luisdegan10@hotmail.com>
2019-03-19 10:54:40 -0300

    get all users

--- data/Queries/users.js
+++ data/Queries/users.js
@@ -1,8 +1,15 @@
-const { GraphQLNonNull } = require('graphql');
-const { UsersType } = require('../Types');
-const Users = require('../Models/users');
+const { GraphQLNonNull, GraphQLList } = require("graphql");
+const { UsersType } = require("../Types");
+const Users = require("../Models/users");
 
 module.exports = {
+  allUsers: {
+    type: new GraphQLList(UsersType),
+    resolve() {
+      return Users.find({});
+    }
+  },
+
   user: {
     type: new GraphQLNonNull(UsersType),
     resolve: async (_, args, { user: { id } }) => {
