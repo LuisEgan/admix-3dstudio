7bd749e91e02a866178914b9f8f9b31a9b6dc047
andmev <tuhouc@gmail.com>
2019-04-25 14:18:09 +0300

    Return Login page

--- pages/login.js
+++ pages/login.js
@@ -1,29 +1,81 @@
-import gql from 'graphql-tag';
-import { Mutation } from 'react-apollo';
+import React, { Component } from 'react';
+import Router from 'next/router';
+import { connect } from 'react-redux';
+import { Formik, Form } from 'formik';
+import isEmail from 'validator/lib/isEmail';
+import actions from '../lib/actions';
 
-export const UPLOAD_FILE = gql`
-  mutation uploadFile($file: Upload!) {
-    uploadModel(model: $file)
+import App from '../components/App';
+import BigImagePanel from '../components/BigImagePanel';
+import TextInput from '../components/inputs/TextInput';
+
+const { login } = actions;
+
+let Login = ({ login, isLoggedIn }) => {
+  const renderFooter = () => {
+    return (
+      <div>
+        <span>Text</span>
+      </div>
+    );
+  };
+
+  const validate = values => {
+    const errors = {};
+    const { email, password } = values;
+
+    if (!email || !isEmail(email)) {
+      errors.email = 'Invalid email';
+    }
+
+    if (!password) {
+      errors.password = 'Please enter a password';
+    }
+
+    return errors;
+  };
+
+  const onSubmit = values => {
+    const { email, password } = values;
+    login(email, password);
+  };
+
+  if (isLoggedIn) {
+    Router.push('/campaigns');
+    return null;
   }
-`;
 
-const uploadOneFile = () => {
   return (
-    <Mutation mutation={UPLOAD_FILE}>
-      {uploadFile => (
-        <input
-          type="file"
-          required
-          onChange={({
-            target: {
-              validity,
-              files: [file],
-            },
-          }) => validity.valid && uploadFile({ variables: { file } })}
-        />
-      )}
-    </Mutation>
+    <App>
+      <BigImagePanel title="Login" footer={renderFooter()}>
+        <Formik initialValues={{ email: '', password: '' }} validate={validate} onSubmit={onSubmit}>
+          {formProps => (
+            <Form>
+              <TextInput name="email" label="Email" {...formProps} />
+              <TextInput name="password" label="Password" {...formProps} />
+              <button type="submit" className="btn gradient-btn" disabled={formProps.isSubmitting}>
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </BigImagePanel>
+    </App>
   );
 };
 
-export default uploadOneFile;
+const mapStateToProps = state => {
+  const { auth } = state;
+  return { ...auth };
+};
+
+const mapDispatchToProps = dispatch => ({
+  login: (email, password) => dispatch(login({ email, password })),
+});
+
+Login = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Login);
+
+export default Login;
